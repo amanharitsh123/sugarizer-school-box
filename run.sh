c1966507eb0fde8ff6026d66332827c01bbd4f8f
Aman Sharma <amanharitsh@debian.amanharitsh>
2018-05-23 02:59:22 -0400

    License Changed

--- run.sh
+++ run.sh
@@ -8,6 +8,13 @@ fi
 
 echo "Starting Script"
 
+if [ -d "build" ]; then
+ rm -r build
+fi
+
+git clone https://github.com/drtyhlpr/rpi23-gen-image.git build/
+
+
 cd build
 
 if [ ! -d "custom.d" ]; then
@@ -17,6 +24,12 @@ fi
 
 #Copying Scripts from ./scripts to /build/custom.d
 find ../scripts -type f -name \* -exec cp {} ./custom.d/ \;
+
+#Copying packages
+cp -r ../packages/* ./files/ 
+
+#Copying Templates
+cp -r ../templates .
 
 #Starting the Build Script
 CONFIG_TEMPLATE="rpi3stretch-sugarizer" ./rpi*
