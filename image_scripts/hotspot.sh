da6eb3e5fe266029926308501aa11fc5e0e0dd61
amanharitsh123 <amanharitsh123@gmail.com>
2018-06-10 11:44:56 +0530

    Hotspot

---
+++ image_scripts/hotspot.sh
@@ -0,0 +1,20 @@
+#!/bin/sh 
+echo "Configuring Hotspot"
+cd /home/pi/RPI3_HOTSPOTS
+
+#Getting Ethernet Device name 
+a=$(ifconfig | grep -wo en.*:)
+
+echo "Device Name is ${a::-1}"
+
+#Replacing all references to eth0 with the ethernet connection name
+sed -i -e "s/eth0/${a::-1}/g" *
+
+#Setting up
+sh install.sh
+
+#starting AP
+ap sugarizer 12345678
+
+sed -i '/^exit/d' /etc/rc.local
+echo -e "ap sugarizer 12345678\nexit 0" >> /etc/rc.local
