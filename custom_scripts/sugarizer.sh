e5a719ebfcd73a3e242bfc89d7ded0ba6f6588c8
amanharitsh123 <amanharitsh123@gmail.com>
2018-07-03 05:19:00 +0000

    Some fixes

--- custom_scripts/sugarizer.sh
+++ custom_scripts/sugarizer.sh
@@ -8,7 +8,7 @@ cp -r ../docker-images ${R}/home/pi/
 
 #Copying all required scripts to the image
 cp ../image_scripts/*.sh ${R}/home/pi/
-cp ../image_scripts/sugarizer ${R}/home/pi/
+cp ../image_scripts/sugarizer_nginx ${R}/home/pi/
 rm ${ETC_DIR}/rc.local
 cp ../image_scripts/rc.local ${ETC_DIR}/
 
@@ -20,5 +20,5 @@ cp -r build/* .
 
 #Changing Hostname
 echo "SugarizerSchoolBox" > ${ETC_DIR}/hostname
-sed -i "s/rpi3-stretch/SugarizerSchoolBox/g" > ${ETC_DIR}/hosts
+sed -i "s/rpi3-stretch/SugarizerSchoolBox/g" ${ETC_DIR}/hosts
 
